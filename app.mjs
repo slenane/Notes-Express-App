7acfb24c87e85c84b9cbdce4000fa5dcbf9ea7dd
slenane <slenane1@gmail.com>
2021-05-28 19:03:01 +0100

    Package update

--- app.mjs
+++ app.mjs
@@ -31,6 +31,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets/vendor/bootstrap', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist')));
+app.use('/assets/vendor/jquery', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
+app.use('/assets/vendor/popper.js', express.static(path.join(__dirname, 'node_modules', 'popper.js', 'dist', 'umd')));
 
 // Router function lists
 app.use('/', indexRouter);
